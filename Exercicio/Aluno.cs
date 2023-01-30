fe59f30be29fee19b228920e9e946af0c415c8ae
Felipe Nascimento <fmfelipe31@gmail.com>
2023-01-30 11:43:56 -0300

    encapsulamento

---
+++ Exercicio/Aluno.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingMethod01
+{
+    internal class Aluno
+    {
+        public double[] Notas = new double [4];
+        public double Media;
+        public double Exame;
+        public double CalcMedia()
+        {
+            Media += ((Notas[0] * 2) + (Notas[1] * 3) + (Notas[2] * 4) + (Notas[3] * 1)) / 10;
+            return Media;
+        }
+
+        public double ValidaAluno()
+        {
+            if (Media >= 7.0)
+            {
+                Console.WriteLine("Aluno Aprovado.");
+            }
+            else if (Media < 5.0)
+            {
+                Console.WriteLine("Aluno reprovado!");
+            }
+            else if (Media > 5.0 || Media <= 6.9)
+            {
+                Console.WriteLine("Aluno em exame.");
+                Exame = double.Parse(Console.ReadLine());
+                Console.WriteLine("Nota do exame: " + Exame);
+                Media = (Exame + Media) / 2;
+                if (Media > 5.0)
+                {
+                    Console.WriteLine("Aluno aprovado.");
+                    
+                }
+                else
+                {
+                    Console.WriteLine("Aluno reprovado.");
+                   
+                }
+            }
+            return Media;
+        }
+    }
+}
---
+++ Exercicio/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TrainingMethod01
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Aluno aluno1 = new Aluno();
+            Console.Write("Nota 1: ");
+            aluno1.Notas[0] = double.Parse(Console.ReadLine());
+            Console.Write("Nota 2: ");
+            aluno1.Notas[1] = double.Parse(Console.ReadLine());
+            Console.Write("Nota 3: ");
+            aluno1.Notas[2] = double.Parse(Console.ReadLine());
+            Console.Write("Nota 4: ");
+            aluno1.Notas[3] = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Media: " + aluno1.CalcMedia());
+
+            Console.WriteLine(aluno1.ValidaAluno());
+
+
+            Console.ReadKey();
+        }
+    }
+}
